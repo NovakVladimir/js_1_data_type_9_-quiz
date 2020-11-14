aa63e75911f219349c7046c870f2d5aadd98db5b
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-14 14:58:28 +0200

    Add js and finish project

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+function checkAnswer() {
+    let result = 0;
+
+    switch(true) {
+        case document.getElementById("year_1").checked===true: 
+            result = result;
+        break;
+        case document.getElementById("year_2").checked===true:
+            result = result + 2;
+        break;
+        case document.getElementById("year_3").checked===true: 
+            result = result;
+        break;
+    }
+
+    switch(true) {
+        case document.getElementById("secYear_1").checked===true: 
+            result = result + 2;
+        break;
+        case document.getElementById("secYear_2").checked===true:
+            result = result;
+        break;
+        case document.getElementById("secYear_3").checked===true: 
+            result = result;
+        break;
+    }
+
+    switch(true) {
+        case document.getElementById("thirdYear_1").checked===true: 
+            result = result;
+        break;
+        case document.getElementById("thirdYear_2").checked===true:
+            result = result + 2;
+        break;
+        case document.getElementById("thirdYear_3").checked===true: 
+            result = result;
+        break;
+    }
+
+    result = `You got  ${result}   points`;
+    document.getElementById("message").innerHTML = result;
+    document.getElementById("message").style.backgroundColor = "rgba(110,200,200,0.8)";
+    return result;
+}
